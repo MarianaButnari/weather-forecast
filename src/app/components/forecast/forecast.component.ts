51deffd482dc011c000de610ed51af66499ea8a7
Mariana <mariana.butnari89@gmail.com>
2024-11-11 11:47:07 +0200

    tweak forecast and location to practice async pipe

--- src/app/components/forecast/forecast.component.ts
+++ src/app/components/forecast/forecast.component.ts
@@ -1,43 +1,37 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {ActivatedRoute, ParamMap, Router, RouterLink} from '@angular/router';
-import {WeatherService} from '../../shared/services/weather.service';
-import {map, switchMap} from 'rxjs';
-import {Forecast} from '../../shared/models/forecast';
-import {NgIf, SlicePipe} from '@angular/common';
-import {TimestampToDatePipe} from '../../shared/pipes/timestamp-to-date.pipe';
-import {ConditionToImgPipe} from '../../shared/pipes/condition-to-img.pipe';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { WeatherService } from '../../shared/services/weather.service';
+import { map, Observable, switchMap } from 'rxjs';
+import { Forecast } from '../../shared/models/forecast';
+import { CommonModule } from '@angular/common';
+import { TimestampToDatePipe } from '../../shared/pipes/timestamp-to-date.pipe';
+import { ConditionToImgPipe } from '../../shared/pipes/condition-to-img.pipe';
 
 @Component({
   selector: 'app-forecast',
   standalone: true,
-  imports: [
-    NgIf,
-    TimestampToDatePipe,
-    ConditionToImgPipe,
-    SlicePipe,
-  ],
+  imports: [CommonModule, TimestampToDatePipe, ConditionToImgPipe],
   templateUrl: './forecast.component.html',
-  styleUrl: './forecast.component.css'
+  styleUrl: './forecast.component.css',
 })
 export class ForecastComponent implements OnInit {
   private router = inject(Router);
   private activatedRoute = inject(ActivatedRoute);
   private weatherService = inject(WeatherService);
-  protected forecast!: Forecast;
+  protected forecast$!: Observable<Forecast>;
 
   ngOnInit(): void {
-    this.activatedRoute.paramMap.pipe(
-      map((params: ParamMap) => params.get('zipcode')!),
-      switchMap((zip: string) => {
-        return this.weatherService.getDailyForecast(zip)
-      })
-    ).subscribe((result: Forecast) => {
-      this.forecast = result;
-    });
+    this.activatedRoute.paramMap
+      .pipe(
+        map((params: ParamMap) => params.get('zipcode')!),
+        switchMap((zip: string) => {
+          return (this.forecast$ = this.weatherService.getDailyForecast(zip));
+        })
+      )
+      .subscribe();
   }
 
   moveBackToHome() {
     this.router.navigate(['']);
   }
-
 }
--- src/app/components/location/location.component.ts
+++ src/app/components/location/location.component.ts
@@ -1,32 +1,36 @@
-import {Component, EventEmitter, inject, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
-import {WeatherService} from '../../shared/services/weather.service';
-import {Location} from '../../shared/models/location';
-import {CommonModule, JsonPipe} from '@angular/common';
-import {ConditionToImgPipe} from '../../shared/pipes/condition-to-img.pipe';
-import {RouterLink} from '@angular/router';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { WeatherService } from '../../shared/services/weather.service';
+import { Location } from '../../shared/models/location';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { ConditionToImgPipe } from '../../shared/pipes/condition-to-img.pipe';
+import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-location',
   standalone: true,
-  imports: [
-    CommonModule,
-    ConditionToImgPipe,
-    RouterLink
-  ],
+  imports: [CommonModule, ConditionToImgPipe, RouterLink],
   templateUrl: './location.component.html',
-  styleUrl: './location.component.css'
+  styleUrl: './location.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LocationComponent implements OnChanges {
   private weatherService = inject(WeatherService);
-  @Input({required: true}) locationZip!: string;
+  @Input({ required: true }) locationZip!: string;
   @Output() panelClosed = new EventEmitter<void>();
-  locationWeather!: Location;
+  protected locationWeather$!: Observable<Location>;
 
   ngOnChanges(changes: SimpleChanges): void {
     const currentZip = changes[`locationZip`]?.currentValue!;
-    this.weatherService.getLocationWeather(currentZip)
-      .subscribe((response: Location) => {
-        this.locationWeather = response;
-      })
+    this.locationWeather$ = this.weatherService.getLocationWeather(currentZip);
   }
 }
